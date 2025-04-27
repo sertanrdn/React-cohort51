e4b50b6c585e7e092decd5ada4fb720a04978748
sertanrdn <sertanerdogan07@gmail.com>
2025-04-27 23:11:45 +0200

    Adding test for cypress

--- week3/prep-exercises/2-e2e-testing/0-myown/2-categories.spec.js
+++ week3/prep-exercises/2-e2e-testing/0-myown/2-categories.spec.js
@@ -52,9 +52,31 @@ describe("Categories", () => {
      * We will give you a couple of steps here, this does not mean that every step is 1 line of code!
      */
     // 1. Check that no category is selected
+    cy.get('[data-testid="categories-list"]').within(() => {
+      cy.get('[data-selected="true"]').should("have.length", 0);
+    });
+
     // 2. Click a category
+    cy.get('[data-testid="categories-list"]').within(() => {
+      cy.get('[data-elementid="electronics"]').click();
+    });
+
     // 3. Check that that category is selected
+    cy.get('[data-testid="categories-list"]').within(() => {
+      cy.get('[data-selected="true"]').should("have.length", 1);
+      cy.get('[data-elementid="electronics"]').should('have.attr', 'data-selected', 'true');
+    });
+
     // 4. Click a different category
+    cy.get('[data-testid="categories-list"]').within(() => {
+      cy.get('[data-elementid="jewelery"]').click();
+    });
+
     // 5. Check that only the new category is selected
+    cy.get('[data-testid="categories-list"]').within(() => {
+      cy.get('[data-selected="true"]').should("have.length", 1);
+      cy.get('[data-elementid="jewelery"]').should('have.attr', 'data-selected', 'true');
+      cy.get('[data-elementid="electronics"]').should('have.attr', 'data-selected', 'false');
+    });    
   });
 });
